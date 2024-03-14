a51279ddc9793d3f130f3b6236d086b3bd351d1b
Service Web <sw@gssmail.com>
2024-03-14 13:55:08 -0500

    Commit changes in C:\Websites\Git\emarlow\5656\GCG_5656_MAINT.sql

---
+++ GCG_5656_MAINT.sql
@@ -0,0 +1,14 @@
+CREATE TABLE "GCG_5656_MAINT"(
+ "DAYS_TO_RUN" INTEGER NOT NULL ,
+ "EMAILS" LONGVARCHAR NOT NULL );
+ 
+/*
+	DAYS_TO_RUN key:
+	1  - Sunday
+	2  - Monday
+	4  - Tuesday
+	8  - Wednesday
+	16 - Thursday
+	32 - Friday
+	64 - Saturday
+*/
